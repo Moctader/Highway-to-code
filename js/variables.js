cd39296f56f2c2fe6e025a9150ea0d5ac5c02101
Moctader <moctader.golam@gmail.com>
2021-01-16 15:59:35 +0200

    new

---
+++ js/variables.js
@@ -0,0 +1,57 @@
+/* 
+    Created on : May, 2019 
+    Author     : Liisa Auer, Oulu University of Applied Sciences
+    Licence    : CC-BY-4.0
+*/
+ 
+/*
+ * Student name :
+ * Started date :
+ * Ended   date :
+ * 
+ */
+
+function t01() {
+
+}
+
+function t02() {        
+    let secInMinutes = 60;
+    let minInHours = 60;
+
+    let secInYear = secInMinutes * minInHours ;    
+    
+}
+
+function t03() {
+
+}
+
+function t04() {
+
+}
+
+
+function t05() {        
+
+}
+
+function t06() {
+
+}
+
+function t07() {
+
+}
+
+function t08() {
+
+}
+
+function t09() {
+
+}
+
+
+
+
